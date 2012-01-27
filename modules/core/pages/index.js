17e437114c4d6daea6265f8849cb34cc0b8d7d23
Artem Shishlov <artem@pushok.com>
2012-01-27 11:15:47 +0400

    made available public directory for mainpage

--- modules/core/pages/index.js
+++ modules/core/pages/index.js
@@ -1,6 +1,9 @@
 var async = require("async");
+var skconnect = require('skilap-connect');
 
 module.exports = function (ctx, app, api, prefix) {
+	
+	app.use(skconnect.vstatic(__dirname + '/../public',{vpath:"/core"}));
 
 	app.get("/", function(req, res, next) {
 		var t = [];
