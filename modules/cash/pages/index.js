dfed2bbe7fdeb6c429488531ef45fb5c659947b6
Sergey Korotkov <sergey@pushok.com>
2012-06-19 22:42:23 +0400

    Batch API prototype

--- modules/cash/pages/index.js
+++ modules/cash/pages/index.js
@@ -1,6 +1,144 @@
 var async = require("async");
 var _ = require('underscore');
 
+var resFunc = {
+	'store': function (obj, key, val, cb) {
+		obj[key]=val;
+		cb();
+	},
+	'clone': function (obj, key, val, cb) {
+		obj[key]=JSON.parse(JSON.stringify(val));
+		cb();
+	},
+	'merge': function (obj, key, val, cb) {
+		_.extend(obj,val);
+		cb();
+	}
+}
+
+var cmdFunc = {
+	'object': function (param, lookup, cb) {
+		cb(null, lookup(param))
+	},
+	'api': function (param, lookup, cb) {
+		var func = param[0].split('.')[1];
+		var args = param.slice(1);
+		var rargs = [];
+		_(args).forEach(function (a) {
+			rargs.push(lookup(a))
+		})
+		rargs.push(cb);
+		var cashapi = lookup("cashapi");
+		cashapi[func].apply(cashapi,rargs);
+	},
+	'pluck': function (param, lookup, cb) {
+		var col = lookup(param[0]);
+		var plu = _(col).pluck(param[1]);
+		if (param[2]) {
+			var u = {};
+			_(plu).forEach(function (e) {
+				if (_.isUndefined(u[JSON.stringify(e)]))
+					u[JSON.stringify(e)]=e;
+			})
+			plu = _(u).values();
+		}
+		cb(null, plu)
+	},
+	'filter': function (param, lookup, cb) {
+		var col = lookup(param[0]);
+		col = _(col).filter(function (e) {
+			return _(param[2]).include(e[param[1]]);
+		})
+		cb(null, col)
+	}	
+}
+
+var Batch = function (cashapi) {
+	var self = this;	
+	var context = {}
+	this.cashapi = cashapi;
+
+	this.lookup = function(a, lctx) {
+		if (!_.isString(a))
+			return a;
+		if (a=="this")
+			return lctx || context;
+		if (a=="cashapi")
+			return self.cashapi;
+		if (!_.isUndefined(context[a]))
+			return context[a]
+		else if (!_.isUndefined(lctx[a]))
+			return lctx[a]
+		else
+			return a;
+	}
+	
+	this.run = function (task,cb) {
+		var ct = {};
+		_(task).forEach(function (v,k) {
+			// little validation 
+			if (_.isUndefined(v.prm))
+				return cb(new Error("Task should have parameter"));			
+			// configure job
+			var job = cmdFunc[v.cmd];
+			if (job==null)
+				return cb(new Error("Unknown job action"));			
+				
+			// configure result
+			var res = {};
+			if (_.isObject(v.res)) _.extend(res,v.res);
+			_.defaults(res,{o:"this",a:"clone",v:"key"});
+			var rf = resFunc[res.a];
+			if (rf==null)
+				return cb(new Error("Unknown result action"));
+			
+			// configure worker function
+			var worker;
+			if (v.ctx==null) {
+				worker = function (cb) {
+					job(v.prm, function (a) { return self.lookup(a) }, function (err, jv) {
+						if (err!=null) return cb(err);
+						rf(self.lookup(res.o), res.v, jv, cb)
+					})
+				}
+			} else if (v.ctx.a == "each") {
+				worker = function (cb) {
+					var col = self.lookup(v.ctx.v);
+					if (!(_.isObject(col) || _.isArray(col)))
+						return cb (new Error ("Bad collection for each"));
+					async.forEach(col, function (e, cb) {
+						job(v.prm, function (a) { return self.lookup(a,e) }, function (err, jv) {
+							if (err!=null) return cb(err);
+							rf(self.lookup(res.o,e), res.v, jv, cb)
+						})
+					}, cb)
+				}
+			}
+			
+			var atask = [];
+			// add dependcies if available
+			if (v.dep) {
+				atask.push(v.dep);
+			}
+			// add safe wrapped worker function
+			atask.push(function (cb) {
+				try {
+					worker(cb);
+				} catch (err) {
+					cb(err)
+				}
+			})
+			ct[k]=atask;
+		})
+		
+		// execute "compiled" batch
+		async.auto(ct, function (err) {
+			if (err!=null) return cb(err);
+			cb(null, context);
+		})
+	}
+}
+
 module.exports = function account(webapp) {
 	var app = webapp.web;
 	var cashapi = webapp.api;
@@ -9,69 +147,104 @@ module.exports = function account(webapp) {
 	var liabilitiesTypes = ["CREDIT", "LIABILITY", "RECEIVABLE", "PAYABLE"];
 	var repCmdty = {space:"ISO4217",id:"RUB"};
 	
-	function getAccountDetails(token, acc, types, callback) {
-		var childs = [];
-		async.auto({
-			get_childs: function(cb1) {
-				getAssets(token, acc.id, types, childs, cb1);
-			},
-			get_details: ['get_childs', function(){
-				cashapi.getAccountInfo(token, acc.id, ["value"], function(err, d) {
-					if (err) return callback(err);
-					var det = {};
-					det.cmdty = acc.cmdty;
-					det.name = acc.name;
-					// do the conversion
-					async.series ([
-						function (cb) {
-							cashapi.getCmdtyPrice(token,det.cmdty,repCmdty,null,null, function (err, rate) {
-								if (err) {
-									return cb(err);
-								}
-								if (!_(repCmdty).isEqual(det.cmdty)) 
-									det.quantity = d.value;
-								det.value = parseFloat(webapp.i18n_cmdtyval(det.cmdty.id,d.value*rate));
-								det.id = acc.id;
-								det.childs = childs;
-								_(childs).forEach (function (e) {
-									det.value+=e.value;
-								})
-								det.fvalue = webapp.i18n_cmdtytext(token,repCmdty,det.value);
-								if (det.quantity)
-									det.fquantity = webapp.i18n_cmdtytext(token,det.cmdty,det.quantity);
-									
-								cb();
-							})
-						}
-					], function (err) {
-						if (err) console.log(err);
-						callback(det);
-					})
+	function getAssets(token, id, types, data, cb) {
+		// filter this level data
+		var level = _(data.accounts).filter(function (e) { return e.parentId == id && _(types).include(e.type); });
+		var res = [];
+		_(level).forEach (function (acc) {
+			var det = {};
+			det.cmdty = acc.cmdty;
+			det.name = acc.name;
+			det.id = acc.id;			
+			getAssets(token, acc.id,types,data, function (err,childs) {
+				if (err) return cb(err);
+				if (!_(repCmdty).isEqual(det.cmdty)) 
+					det.quantity = acc.value;
+				var rate = 1;
+				var r = _(data.cmdty).find(function (e) { return e.id==acc.cmdty.id });
+				if (r!=null)
+					rate = r.rate;
+				det.value = parseFloat(webapp.i18n_cmdtyval(det.cmdty.id,acc.value*rate));
+				det.childs = childs;
+				_(childs).forEach (function (e) {
+					det.value+=e.value;
 				})
-			}]
-		});
-	};
-
-	function getAssets(token, id, types, assets_, callback) {
-		cashapi.getChildAccounts(token, id, function(err, accounts) {
-			if (err) return callback(err);
-			async.forEachSeries(accounts, function(acc, cb2){
-				getAccountDetails(token, acc, types, function(det1) {
-					if (_.indexOf(types, acc.type) !=-1) {
-						assets_.push(det1);
-					}
-					cb2();
-				});
-			}, callback);
-		});
+				det.fvalue = webapp.i18n_cmdtytext(token,repCmdty,det.value);
+				if (det.quantity)
+					det.fquantity = webapp.i18n_cmdtytext(token,det.cmdty,det.quantity);
+				res.push(det);
+			})
+		})
+		cb(null, res);
 	}
 
 	app.get(prefix, function(req, res, next) {
+		var data;
 		var assets = [];
 		var liabilities = [];
 		async.waterfall([
-			function (cb) { getAssets(req.session.apiToken, 0, assetsTypes, assets, cb) },
-			function (cb) { getAssets(req.session.apiToken, 0, liabilitiesTypes, liabilities, cb) },
+			function (cb) {
+				var batch = {
+					"setup":{
+						"cmd":"object",
+						"prm":{"token":req.session.apiToken,"repCmdty":repCmdty},
+						"res":{"a":"merge"}
+					},
+					"accounts":{
+						"dep":"setup",
+						"cmd":"api",
+						"prm":["cashapi.getAllAccounts","token"],
+						"res":{"a":"store","v":"accounts"}
+					},
+					"filter":{
+						"dep":"accounts",
+						"cmd":"filter",
+						"prm":["accounts","type",["BANK", "CASH", "ASSET", "STOCK", "MUTUAL", "CURENCY","CREDIT", "LIABILITY", "RECEIVABLE", "PAYABLE"],"IN"],
+						"res":{"a":"store","v":"accounts"}
+					},					
+					"info":{
+						"dep":"filter",
+						"cmd":"api",
+						"ctx":{"a":"each","v":"accounts"},
+						"prm":["cashapi.getAccountInfo","token","id",["value"]],
+						"res":{"a":"merge"}
+					},
+					"cmdty":{
+						"dep":"filter",
+						"cmd":"pluck",
+						"prm":["accounts","cmdty","unique"],
+						"res":{"a":"clone","v":"cmdty"}
+					},
+					"rates":{
+						"dep":"cmdty",
+						"cmd":"api",
+						"ctx":{"a":"each","v":"cmdty"},	
+						"prm":["cashapi.getCmdtyPrice","token","this","repCmdty",null,null],
+						"res":{"a":"store","v":"rate"}
+					}					
+				}
+				
+				var b = new Batch(cashapi);
+				b.run(batch,function (err, _data) {
+					if (err) return scb(err);
+					data = _data;
+					cb();
+				})
+			},
+			function (cb) { 
+				getAssets(req.session.apiToken, 0, assetsTypes, data, function (err, res) {
+					if (err) return cb(err);
+					assets = res;
+					cb();
+				})
+			},
+			function (cb) { 
+				getAssets(req.session.apiToken, 0, liabilitiesTypes, data, function (err, res) {
+					if (err) return cb(err);
+					liabilities = res;
+					cb();
+				})
+			},
 			function (cb) { webapp.guessTab(req, {pid:'home',name:'Home',url:req.url}, cb) },
 			function render (vtabs) {
 				var rdata = {settings:{views:__dirname+"/../views"},prefix:prefix, tabs:vtabs};
