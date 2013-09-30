38598a287fb57522d1889988f8fd4e8566c5825e
Ilya Shatokhin <Ilyas@pushok.com>
2013-09-30 09:14:50 +0100

    fix

--- compile.sh
+++ compile.sh
@@ -9,22 +9,22 @@ rm -rf $BUILD_FOLDER
 mkdir $BUILD_FOLDER
 
 mkdir $BUILD_FOLDER/modules
-cp -r modules/* $BUILD_FOLDER/modules
+cp -r -L modules/* $BUILD_FOLDER/modules
 
 mkdir $BUILD_FOLDER/data
-cp -r data/* $BUILD_FOLDER/data
+cp -r -L data/* $BUILD_FOLDER/data
 
 mkdir $BUILD_FOLDER/node_modules
-cp -r node_modules/* $BUILD_FOLDER/node_modules
+cp -r -L node_modules/* $BUILD_FOLDER/node_modules
 
 mkdir $BUILD_FOLDER/public
-cp -r public/* $BUILD_FOLDER/public
+cp -r -L public/* $BUILD_FOLDER/public
 
 mkdir $BUILD_FOLDER/test
-cp -r test/* $BUILD_FOLDER/test
+cp -r -L test/* $BUILD_FOLDER/test
 
 mkdir $BUILD_FOLDER/res
-cp -r res/* $BUILD_FOLDER/res
+cp -r -L res/* $BUILD_FOLDER/res
 
 mkdir $BUILD_FOLDER/logs
 
